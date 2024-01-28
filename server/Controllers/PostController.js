f18ae8539d4d15d2c32edea06c1f8e1c9d76124d
harshit Mishra <miananya2@gmail.com>
2024-01-28 19:08:54 +0530

    get post

--- server/Controllers/PostController.js
+++ server/Controllers/PostController.js
@@ -59,4 +59,27 @@ export const createPost = async (req, res) => {
   }
 };
 
+export const getPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
 
+    if (!post) {
+      return res.status(404).json({
+        status: "Failed",
+        message: "Post does not exist",
+      });
+    }
+
+    res.status(200).json({
+      status: "Success",
+      data: {
+        post,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "Failed",
+      message: "Something went wrong",
+    });
+  }
+};
